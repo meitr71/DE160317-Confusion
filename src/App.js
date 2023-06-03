fe042253507f61afb6943651b77dd8ad3f461d5f
maitran <maitttde160317@fpt.edu.vn>
2023-06-03 16:22:05 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +22,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        {/* <Menu /> */}
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
